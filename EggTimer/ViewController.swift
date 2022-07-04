fae8899e3ef748234e615e6fb008cc64ee80c6eb
Siam300 <siamkhan8523@gmail.com>
2022-07-03 23:21:49 -0700

    Added Hardness Time

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,7 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let eggTimes : [String : Int] = ["Soft" : 5, "Medium" : 7, "Hard" : 12]
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime  = 12
     
-
+    @IBAction func hardnessScale(_ sender: UIButton) {
+        let hardness = sender.currentTitle
+        
+//        if(sender.currentTitle == "Soft"){
+//            print(softTime)
+//        }
+//
+//        else if(sender.currentTitle == "Medium"){
+//            print(mediumTime)
+//        }
+//
+//        else{
+//            print(hardTime)
+//        }
+        
+        
+//        switch hardness {
+//        case "Soft":
+//            print(softTime)
+//        case "Medium":
+//            print(mediumTime)
+//        default:
+//            print(hardTime)
+//        }
+        if (sender.currentTitle == eggTimes) {
+            print(eggTimes)
+        }
+}
+    
 
 }
